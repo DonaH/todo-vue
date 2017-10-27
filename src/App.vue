72ee5a8642720f40ba4f99a896ad165ee468eb01
d0nah <dona@gooduniversity.com>
2017-10-27 16:28:35 -0700

    completed version of the Todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import sweetalert from 'sweetalert';
+  import TodoList from './components/TodoList';
+  import CreateTodo from './components/CreateTodo';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'app',
+    components: {
+      TodoList,
+      CreateTodo,
+    },
+    data() {
+      return {
+        todos: [{
+          title: 'Todo A',
+          project: 'Project A',
+          done: false,
+        }, {
+          title: 'Todo Is Done',
+          project: 'Project B',
+          done: true,
+        }, {
+          title: 'New Vue App Needed',
+          project: 'Project C',
+          done: false,
+        }, {
+          title: 'Light At The End Of The Tunnel',
+          project: 'Project D',
+          done: false,
+        }],
+      };
+    },
+    methods: {
+      createTodo(newTodo) {
+        this.todos.push(newTodo);
+        sweetalert('Success!', 'To-Do created!', 'success');
+      },
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
